09c9883dc926908071ed11b374d5439d1d383cd1
Matthew Oluwabusayo Opoola <initoluwa@gmail.com>
2023-04-19 23:32:28 +0100

    Update Trending.jsx

--- src/pages/home/trending/Trending.jsx
+++ src/pages/home/trending/Trending.jsx
@@ -19,7 +19,7 @@ const Trending = () => {
         <div className="carouselSection">
             <ContentWrapper>
                 <span className="carouselTitle">Trending</span>
-                <SwitchTabs data={["Day", "Week","Month"]} onTabChange={onTabChange} />
+                <SwitchTabs data={["Day", "Week"]} onTabChange={onTabChange} />
             </ContentWrapper>
             <Carousel data={data?.results} loading={loading} />
         </div>
